3ef284eab5f6ce7060e63b2468ec272960b112f0
Joel Maldonado Fernandez <106793712+JoelMaldonado@users.noreply.github.com>
2026-05-30 18:56:26 -0500

    feat: cambiar modo de ejecución a 'fork' en la configuración de ecosistema

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
       script: 'node_modules/.bin/next',
       args: 'start',
       cwd: '/var/www/vec-admin',
-      instances: 1,
+      exec_mode: 'fork',
       autorestart: true,
       watch: false,
       env: {
